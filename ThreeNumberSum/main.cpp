9045a445d14757a9858ac992304be2e5513a69cc
Adeeb Mohammed <adeeb2358@gmail.com>
2020-01-06 06:02:38 +0900

    Initial Commit

---
+++ ThreeNumberSum/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  ThreeNumberSum
+//
+//  Created by adeeb mohammed on 06/01/20.
+//  Copyright © 2020 adeeb mohammed. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
